5cf968159ae861083885ceb5b95e18f60498080b
leo2971998 <106453722+leo2971998@users.noreply.github.com>
2022-08-01 13:40:04 -0500

    Updated

--- src/Classes/Contact.java
+++ src/Classes/Contact.java
@@ -1,2 +1,96 @@
-package Classes;public class Contact {
+package Classes;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Contact {
+    private String name;
+    private String company;
+    private String title;
+    private String mobile;
+    private String alternateMobile;
+    private String email;
+    private LocalDate dateCreated;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAlternateMobile() {
+        return alternateMobile;
+    }
+
+    public void setAlternateMobile(String alternateMobile) {
+        this.alternateMobile = alternateMobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Contact() {
+    }
+    public Contact(String name, String company, String title, String mobile, String alternateMobile, String email, LocalDate dateCreated) {
+        this.name = name;
+        this.company = company;
+        this.title = title;
+        this.mobile = mobile;
+        this.alternateMobile = alternateMobile;
+        this.email = email;
+        this.dateCreated = dateCreated;
+    }
+    public static Contact CreateContact(String details) {
+        String[] str = new String[7];
+        str = details.split(",");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        Contact C = new Contact(str[0], str[1], str[2], str[3], str[4], str[5], LocalDate.parse(str[6], formatter));
+        return C;
+    }
+    public String Display() {
+        return String.format("%5s %20s %20s %20s %20s %20s %20s", name, company, title, mobile, alternateMobile, email, dateCreated);
+    }
+
+    public String getDomain() {
+        return email.substring(email.indexOf("@") + 1, email.indexOf("."));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import Classes.*;
+
+import java.sql.Array;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.time.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of contact details: ");
+        int num = sc.nextInt();
+        sc.nextLine();
+        List<Contact> C = new ArrayList<Contact>();
+        for (int i = 0; i < num; i++) {
+            C.add(Contact.CreateContact(sc.nextLine()));
+        }
+        System.out.println("Enter a sort type: ");
+        System.out.println("1. Sort by name");
+        System.out.println("2. Sort by email domain");
+        System.out.println("3. Sort by date created");
+        int choice = sc.nextInt();
+        sc.nextLine();
+        switch (choice) {
+            case 1:
+                C.stream().sorted((Comparator.comparing(Contact::getName))).forEach(T-> System.out.println(T.Display()));
+                break;
+            case 2:
+                C.stream().sorted((Comparator.comparing(Contact::getDomain))).forEach(T-> System.out.println(T.Display()));
+                break;
+            case 3:
+                C.stream().sorted((Comparator.comparing(Contact::getDateCreated))).forEach(T-> System.out.println(T.Display()));
+                break;
+            default:
+                System.out.println("Invalid choice.");
+                break;
+        }
     }
 }
